1443c24a1a5d21368e59369c7bfc3f2245dbaa2a
Animol N R <animol698@gmail.com>
2020-07-02 16:14:31 +0530

    Updates

---
+++ precache-manifest.1f276db51820ce493902f2069cd43993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1fd98f72967250785d0a28645c0a6f",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "9bcc5c0b425469621c16",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6d66bc7815dea5b307e",
+    "url": "/deploy-react/static/js/2.6b121d5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.6b121d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcc5c0b425469621c16",
+    "url": "/deploy-react/static/js/main.fc8016c3.chunk.js"
+  },
+  {
+    "revision": "2ab7a2a233d61f35914f",
+    "url": "/deploy-react/static/js/runtime-main.afd4d9c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
